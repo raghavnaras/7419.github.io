8b84c86c95a7449226426218bf9d8a3b26b70ba8
Raghav <raghavnaras01@gmail.com>
2018-09-28 22:13:10 -0700

    email change

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "https://formspree.io/drj.qls@gmail.com",
+        url: "https://formspree.io/support@7419.tech",
         method: "POST",
         data: {
           name: name,
